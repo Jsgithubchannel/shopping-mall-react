5a6e1aef1b2d62fc0253773a0a9872a74553acb8
Jsgithubchannel <jsschannel99@gmail.com>
2021-07-27 14:46:06 +0900

    design: add styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import Navbar from "./Navbar";
 import Jumbotron from "./Jumbotron";
 
@@ -10,26 +10,29 @@ function App() {
 
       <div className="container">
         <div className="row">
-          <div className="products">
+          <div className="product">
             <img
               src="https://images.unsplash.com/photo-1544022613-e87ca75a784a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"
               alt="jacket"
-              width="100%"
             />
+            <h4>상품명</h4>
+            <p>상품정보</p>
           </div>
           <div className="product">
             <img
               src="https://images.unsplash.com/photo-1580331451062-99ff652288d7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"
               alt="sweater"
-              width="100%"
             />
+            <h4>상품명</h4>
+            <p>상품정보</p>
           </div>
           <div className="product">
             <img
               src="https://images.unsplash.com/photo-1584998316204-3b1e3b1895ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=80"
               alt="skirt"
-              width="100%"
             />
+            <h4>상품명</h4>
+            <p>상품정보</p>
           </div>
         </div>
       </div>
