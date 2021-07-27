1bad75cd8fe4e2fab9f4e23eec6455f714073630
Jsgithubchannel <jsschannel99@gmail.com>
2021-07-27 13:25:40 +0900

    feat: add jumbotron style, html tags

--- src/Jumbotron.jsx
+++ src/Jumbotron.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "./Jumbotron.scss";
 
 const Jumbotron = () => {
-  return <div></div>;
+  return (
+    <div className="cover">
+      <h1 className="jumbotron__title">20% Season Off</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, ut?
+      </p>
+      <button>Learn More</button>
+    </div>
+  );
 };
 
 export default Jumbotron;
