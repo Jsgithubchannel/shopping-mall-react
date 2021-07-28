aa5b5545182ef841a53beb1e4fdeb6655e7e73d8
Jsgithubchannel <jsschannel99@gmail.com>
2021-07-28 14:11:42 +0900

    feat: add alert popup, useEffect

--- src/Components/Detail.jsx
+++ src/Components/Detail.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
 
 const Detail = (props) => {
+  const [alert, setAlert] = useState(true);
   let history = useHistory();
   let { id } = useParams();
   let foundedProduct = props.product.find((e) => {
     return e.id == id;
   });
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [alert]);
+
   return (
     <div className="container">
       <div className="row">
@@ -14,6 +25,9 @@ const Detail = (props) => {
           <img src="https://images.unsplash.com/photo-1544022613-e87ca75a784a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80" />
         </div>
         <div className="col-md-6 mt-4">
+          {alert === true ? (
+            <div className="alert-popup">재고가 얼마 남지 않았습니다.</div>
+          ) : null}
           <h4 className="pt-5">{foundedProduct.title}</h4>
           <p>{foundedProduct.content}</p>
           <p>{foundedProduct.price}</p>
