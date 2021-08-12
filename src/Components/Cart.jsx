bf1ba6b12955b970ba81fd5132b1f0d198a35e92
Jsgithubchannel <jsschannel99@gmail.com>
2021-08-13 00:24:27 +0900

    fix: fix cart

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { Table, Alert } from "react-bootstrap";
-import { connect, useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const Cart = (props) => {
   const state = useSelector((state) => state); //redux에 있는 모든 state
-  console.log(state.reducer);
   const dispatch = useDispatch();
 
   return (
@@ -32,8 +31,6 @@ const Cart = (props) => {
                           type: "수량증가",
                           payload: {
                             id: val.id,
-                            name: val.name,
-                            quan: val.quan,
                           },
                         });
                       }}
@@ -46,8 +43,6 @@ const Cart = (props) => {
                           type: "수량감소",
                           payload: {
                             id: val.id,
-                            name: val.name,
-                            quan: val.quan,
                           },
                         });
                       }}
@@ -62,12 +57,10 @@ const Cart = (props) => {
         </tbody>
       </Table>
 
-      {props.alert열렸니 ? (
+      {state.reducer2 ? (
         <Alert variant={"primary"}>
           <p>지금 구매하시면 20% 할인</p>
-          <button onClick={() => props.dispatch({ type: "alert닫기" })}>
-            닫기
-          </button>
+          <button onClick={() => dispatch({ type: "alert닫기" })}>닫기</button>
         </Alert>
       ) : null}
     </div>
